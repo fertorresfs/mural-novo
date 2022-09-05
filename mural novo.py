099d0b4a75749710617de49fe6d68365e8f9f692
Fernando Torres Ferreira da Silva <62218105+fertorresfs@users.noreply.github.com>
2022-09-05 20:51:56 -0300

    Update mural novo.py

--- mural novo.py
+++ mural novo.py
@@ -2,5 +2,7 @@ from time import sleep
 
 print("Felipe Paixão")
 print("Espero que eu já esteja para virar um cientista de dados!")
-print("Thais Faustino: espero estar formada em TI e trabalhando como programadora há um boom tempo")
+print("Júlio César")
+print("Esperto que eu esteja empregado em uma empresa de TI até o final do curso!")
+print("Meu nome é Fernando Torres, daqui 3 anos, espero me tornar um cienctista de dados!!!")
 sleep(1)
